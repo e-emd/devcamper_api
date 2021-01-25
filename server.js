50a2554590d4a00770eb772e351e7a4de53078e5
e-emd <dewin.emd@gmail.com>
2021-01-25 16:00:13 -0700

    Bootcamps middlewares

--- server.js
+++ server.js
@@ -3,25 +3,34 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
+// Load env vars
 dotenv.config({
   path: './config/config.env',
 });
 
+// Connect to database
 connectDB();
 
+// Route files
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
 app.use(express.json());
 
+// Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
